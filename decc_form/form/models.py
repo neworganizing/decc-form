e63f53c90c6bb28c0b9fa71fb6287dcdebc77b7b
Jenna Colazzi <jenna@neworganizing.com>
2014-04-24 11:21:57 -0400

    fixed incrementing problem

--- decc_form/form/models.py
+++ decc_form/form/models.py
@@ -101,30 +101,18 @@ class Batch(models.Model):
     processed_date = models.DateField(null=True, blank=True)
     return_date = models.DateField(null=True, blank=True)
 
-    """
     def save(self, *args, **kwargs):
-        if not self.pk:
-            self.submission_date = datetime.datetime.today()
-        super(Batch, self).save(*args, **kwargs)
-        #if self.vendor_filename == None: 
-        #    self.part.order.project.id 
-    """
-    def save(self, *args, **kwargs):
-        print 'saving'
         if not self.pk:
             proj_id = self.part.order.project.id
             c_id = str(Client.objects.get(project=proj_id).id)
-            print 'c_id: {}'.format(c_id)
             while len(c_id) < 3:
                 c_id = '0' + c_id
             try:
-                b_id = str(Batch.objects.filter(id__startswith=c_id).order_by('-id')[0].id)
+                b_id = str(Batch.objects.filter(id__startswith=int(c_id)).order_by('-id')[0].id + 1)[-7:] 
             except (Batch.DoesNotExist, IndexError) as e:
                 b_id = str(1)
             while len(b_id) < 7:
                 b_id = '0' + b_id
-            print 'c_id: {}'.format(c_id)
-            print 'b_id: {}'.format(b_id)
             self.vendor_filename = c_id + b_id + '.pdf'
             self.id = int(c_id + b_id)
             self.submission_date = datetime.datetime.today()
