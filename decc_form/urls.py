452bd4a4893f956929871ed3cfe1b75c8593fa62
Jenna Colazzi <jenna@neworganizing.com>
2014-04-25 13:07:07 -0400

    no more migrations

--- decc_form/urls.py
+++ decc_form/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import patterns, include, url
-from form import views
+import views
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^order/(?P<order_id>[0-9]+)/part/(?P<part_id>[0-9]+)/batch/$', views.BatchView.as_view(template_name='form/batch_form.html'), name='batch'),
-    url(r'^order/(?P<order_id>[0-9]+)/part/$', views.PartView.as_view(template_name='form/part_form.html'), name='part'),
-    url(r'^order/$', views.OrderView.as_view(template_name='form/order_form.html'), name='order'),
-    url(r'^thanks/$', views.ThanksView.as_view(template_name='form/thanks.html'), name='thanks'),
+    url(r'^order/(?P<order_id>[0-9]+)/part/(?P<part_id>[0-9]+)/batch/$', views.BatchView.as_view(template_name='decc_form/batch_form.html'), name='batch'),
+    url(r'^order/(?P<order_id>[0-9]+)/part/$', views.PartView.as_view(template_name='decc_form/part_form.html'), name='part'),
+    url(r'^order/$', views.OrderView.as_view(template_name='decc_form/order_form.html'), name='order'),
+    #url(r'^thanks/$', views.ThanksView.as_view(template_name='form/thanks.html'), name='thanks'),
 
 
     url(r'^admin/', include(admin.site.urls)),
