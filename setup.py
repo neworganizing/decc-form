609e6b647c0f3d34a4ea727210afef3697e1c95a
Jenna Colazzi <jenna@neworganizing.com>
2014-04-25 13:12:21 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DECC',
+    version='0.1',
+    packages=['decc_form,'],
+    license='copyright NOI',
+    long_description='DECC Form Submission Website',
+)
