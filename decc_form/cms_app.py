028fa5b02591390be3caf3dc9aef54950f81af4a
Jenna Colazzi <jenna@neworganizing.com>
2014-04-25 13:20:49 -0400

    app hook

--- decc_form/cms_app.py
+++ decc_form/cms_app.py
@@ -1,8 +1,8 @@
-from cms.app_base import decc_form
+from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-class DeccAppHook(decc_form):
+class DeccAppHook(CMSApp):
     name = _('DECC Apphook')
     urls = ['decc_form.urls']
 
