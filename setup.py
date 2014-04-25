32475a78fc5ea206ac45beb19b9a1934cf8f3f72
Jenna Colazzi <jenna@neworganizing.com>
2014-04-25 13:18:50 -0400

    setup change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='decc-form',
     version='0.1',
-    packages=['decc_form,'],
+    packages=['decc_form',],
     license='copyright NOI',
     long_description='DECC Form Submission Website',
 )
