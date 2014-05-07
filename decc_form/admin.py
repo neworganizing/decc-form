efb760ccf4e6b79039777274e4c6a5f0602d0f59
Tim Anderegg <timothy.anderegg@gmail.com>
2014-05-07 11:13:25 -0700

    Added order_frequency to admin

--- decc_form/admin.py
+++ decc_form/admin.py
@@ -25,7 +25,7 @@ class AddressAdmin(admin.ModelAdmin):
     ordering = ('state',)
 
 class ProjectAdmin(admin.ModelAdmin):
-    fields = ['billable', 'start_date', 'end_date', 'estimated_item_count', 'notes']
+    fields = ['billable', 'start_date', 'end_date', 'estimated_item_count', 'notes', 'order_frequency']
     list_display = ['id', 'start_date', 'end_date']
 
     
